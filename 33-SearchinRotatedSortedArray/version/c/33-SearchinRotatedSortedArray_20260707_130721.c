3e96a5e6f8e9e89a06655b3bc0b581c7cc9e16e6
temp-pie <naman16101237@gmail.com>
2026-07-07 13:07:21 +0530

    Create 33-SearchinRotatedSortedArray

---
+++ 33-SearchinRotatedSortedArray/version/c/33-SearchinRotatedSortedArray_20260707_130721.c
@@ -0,0 +1,14 @@
+// Last updated: 07/07/2026, 13:07:21
+1int search(int* nums, int numsSize, int target) {
+2    int found=0;
+3    int pos;
+4    for(int i=0;i<numsSize;i++){
+5        if(nums[i]==target){
+6            found=1;
+7            pos=i;
+8        }
+9    }
+10    if(found==1){
+11    return pos;}
+12    else return -1;
+13}
