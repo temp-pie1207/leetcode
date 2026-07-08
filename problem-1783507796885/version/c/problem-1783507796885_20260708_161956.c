7aac55402df25a844d3a9fec2de3d5e2a5e843c8
temp-pie <naman16101237@gmail.com>
2026-07-08 16:19:56 +0530

    Create

---
+++ problem-1783507796885/version/c/problem-1783507796885_20260708_161956.c
@@ -0,0 +1,32 @@
+// Last updated: 08/07/2026, 16:19:56
+1/**
+2 * Definition for singly-linked list.
+3 * struct ListNode {
+4 *     int val;
+5 *     struct ListNode *next;
+6 * };
+7 */
+8
+9struct ListNode *detectCycle(struct ListNode *head) {
+10
+11    struct ListNode *slow = head;
+12    struct ListNode *fast = head;
+13
+14    while (fast != NULL && fast->next != NULL) {
+15        slow = slow->next;
+16        fast = fast->next->next;
+17
+18        if (slow == fast) {
+19            slow = head;
+20
+21            while (slow != fast) {
+22                slow = slow->next;
+23                fast = fast->next;
+24            }
+25
+26            return slow;
+27        }
+28    }
+29
+30    return NULL;
+31}
