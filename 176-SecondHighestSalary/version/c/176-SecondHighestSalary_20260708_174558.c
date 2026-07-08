72df42e136b16a67d6b3fbf5f4bc9b1b96b13524
temp-pie <naman16101237@gmail.com>
2026-07-08 17:45:58 +0530

    Create 176-SecondHighestSalary

---
+++ 176-SecondHighestSalary/version/c/176-SecondHighestSalary_20260708_174558.c
@@ -0,0 +1,11 @@
+// Last updated: 08/07/2026, 17:45:58
+1int findMin(int* nums, int numsSize) {
+2    int min=nums[0];
+3    for(int i =1;i<numsSize;i++){
+4        if (nums[i]<min){
+5            min=nums[i];
+6        }
+7    }
+8    return min;
+9    
+10}
