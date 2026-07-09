2f2f2beceea28e6cdd3ab80d6a2da188e31df044
temp-pie <naman16101237@gmail.com>
2026-07-09 14:00:22 +0530

    Create 344-ReverseString

---
+++ 344-ReverseString/version/c/344-ReverseString_20260709_140022.c
@@ -0,0 +1,27 @@
+// Last updated: 09/07/2026, 14:00:22
+1bool isVowel(char c) {
+2    return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+3           c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+4}
+5
+6char* reverseVowels(char* s) {
+7    int left = 0;
+8    int right = strlen(s) - 1;
+9
+10    while (left < right) {
+11        while (left < right && !isVowel(s[left]))
+12            left++;
+13
+14        while (left < right && !isVowel(s[right]))
+15            right--;
+16
+17        char temp = s[left];
+18        s[left] = s[right];
+19        s[right] = temp;
+20
+21        left++;
+22        right--;
+23    }
+24
+25    return s;
+26}
