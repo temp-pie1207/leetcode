758079c0b4b97f90b547620c152c0e3ce5ebaa0e
temp-pie <naman16101237@gmail.com>
2026-07-21 16:01:05 +0530

    Create 1470-ShuffletheArray

---
+++ 1470-ShuffletheArray/version/c/1470-ShuffletheArray_20260721_160110.c
@@ -0,0 +1,18 @@
+// Last updated: 21/07/2026, 16:01:10
+1/**
+2 * Note: The returned array must be malloced, assume caller calls free().
+3 */
+4int* shuffle(int* nums, int numsSize, int n, int* returnSize) {
+5
+6    int *ans = (int *)malloc(sizeof(int) * numsSize);
+7
+8    int j = 0;
+9
+10    for (int i = 0; i < n; i++) {
+11        ans[j++] = nums[i];
+12        ans[j++] = nums[i + n];
+13    }
+14
+15    *returnSize = numsSize;
+16    return ans;
+17}
